cb070d8f157a5da9441b6852c093b460bb981efb
Dmitry Bystrov <98824571+Artoym715@users.noreply.github.com>
2024-06-18 15:52:24 +0500

    Del Mail

--- mail.php
+++ mail.php
@@ -6,19 +6,20 @@ $mail = new PHPMailer;
 $mail->CharSet = 'UTF-8';
 
 
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    function test_input($data) {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    function test_input($data)
+    {
         $data = trim($data);
         $data = stripslashes($data);
         $data = htmlspecialchars($data);
         return $data;
     }
-    
+
     $name = $telephone = "";
     $name = test_input($_POST['name']);
     $telephone = $_POST['telephone'];
 
-$message = '
+    $message = '
 <html>
 <head>
     <title>Заявка с сайта</title>
@@ -26,14 +27,14 @@ $message = '
 <body>
 <h2 style="margin: 0; padding: 20px; color: #ffffff; background: #4b9fc5;">Заявка с сайта</h2>
 <div style="background: #fafafa; padding: 20px; color: #333333;">
-    <p><b>Имя:</b> '. $name.' </p>
-    <p><b>Телефон:</b> '.$telephone.'</p> 
+    <p><b>Имя:</b> ' . $name . ' </p>
+    <p><b>Телефон:</b> ' . $telephone . '</p> 
 </div>                      
 </body>
 </html>
 
 ';
-    $admin_email ='Danilkorotkov87@mail.ru';
+    $admin_email = '';
 
     $mail->setFrom('adm@' . $_SERVER['HTTP_HOST'], 'avtogas.kz'); // от кого будет уходить письмо?
     $mail->addAddress($admin_email);     // Кому будет уходить письмо
@@ -43,9 +44,6 @@ $message = '
     $mail->msgHTML($body);
 
     $mail->send();
-
-}
-else{
+} else {
     header('location: /');
 }
-?>
